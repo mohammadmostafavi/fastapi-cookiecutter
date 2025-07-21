2e4f7806b2e2cb242939b5231f3d63b22fc6c632
Mohammad <mohammad@mohammadmostafavi.com>
2025-07-21 13:46:08 +0330

    Add support for optional Celery integration and update Docker Compose files

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -4,6 +4,7 @@ import shutil
 
 licence = "{{cookiecutter.license}}"
 project_slug = "{{cookiecutter.project_slug}}"
+use_celery = "{{cookiecutter.use_celery}}"
 
 
 def delete_resource(resource):
@@ -17,3 +18,8 @@ def delete_resource(resource):
 
 if licence == "None":
     delete_resource("LICENSE")
+
+if use_celery == "n":
+    delete_resource("{{cookiecutter.project_slug}}/src/celery.py")
+    delete_resource("{{cookiecutter.project_slug}}/apps/user/tasks.py")
+    delete_resource("{{cookiecutter.project_slug}}/apps/logs/tasks.py")
